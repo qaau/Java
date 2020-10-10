815be2c9154dbb3319f6b399d4e3372ca65d4f41
vitaliy <70757477+qaau@users.noreply.github.com>
2020-10-10 20:08:53 +0300

    ignore update

--- sandbox/src/Main/java/JavaProject/MyFirstProgram.java
+++ sandbox/src/Main/java/JavaProject/MyFirstProgram.java
@@ -5,12 +5,13 @@ public class MyFirstProgram {
     public static void main (String[] args) {
         hello("you");
         hello("Young Padavan");
+        hello("me");
 
         Square s = new Square(6);
         //s.l = 5;
         System.out.println("Square area with side " + s.l + " equals " + s.area());
 
-        Rectangle r = new Rectangle(5,6);
+        Rectangle r = new Rectangle(12,13);
         /*r.a = 5;
         r.b = 6;*/
         System.out.println("Rectangle area with sides " + r.a + " and " + r.b +" equals " + r.area());
